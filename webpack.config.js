21965d18a95bd128fefb84a51c1196702a5ed3bf
Mike Holguin <mholguin@mammothwebworks.com>
2020-01-14 20:46:03 -0800

    Consolidated Webpack config files

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,72 @@
-const environment = (process.env.NODE_ENV || 'development').trim();
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
-if (environment === 'development') {
-    module.exports = require('./config/webpack.dev.config.js');
-} else {
-    module.exports = require('./config/webpack.prod.config.js');
-}
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve( __dirname, '../dist' ),
+        filename: '[name].[hash].js'
+    },
+    module: {
+      rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: [
+                    { loader: 'babel-loader', options: {} }
+                ]
+            },
+            {
+                test: /\.(sa|sc|c)ss$/,
+                use: [
+                    MiniCssExtractPlugin.loader, 
+                    { loader: 'css-loader', options: {} },
+                    { 
+                        loader: 'postcss-loader', 
+                        options: {
+                            ident: 'postcss',
+                            plugins: [
+                                require('autoprefixer')({
+                                    'browsers': ['> 1%', 'last 2 versions']
+                                }),
+                            ]
+                        }
+                    },
+                    { loader: 'sass-loader', options: {} },
+                ]
+            }
+        ]
+    },
+    devServer: {
+        port: 8080,
+        contentBase: ['./src', './dist'],
+        writeToDisk: false,
+        inline: true,
+        hot: true
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            title: 'JavaScript To-do List',
+            template: './src/index.html',
+            inject: true,
+            minify: {
+                removeComments: true,
+                collapseWhitespace: false
+            }
+        }),
+        new MiniCssExtractPlugin({
+            filename: 'style.[chunkhash].css'
+        }),
+        new CleanWebpackPlugin(),
+    ],
+    optimization: {   
+        minimizer: [     
+            new UglifyJsPlugin(),     
+            new OptimizeCSSAssetsPlugin()   
+        ] 
+    }
+};
